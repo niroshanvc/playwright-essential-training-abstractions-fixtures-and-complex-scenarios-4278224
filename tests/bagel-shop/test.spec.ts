4639f911d06b6b8ffb04abd7386852c165675370
Niroshan - CareHires <niroshan.desilva@carehires.com>
2026-07-14 21:54:21 +0530

    timeouts

--- tests/bagel-shop/test.spec.ts
+++ tests/bagel-shop/test.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("Bagel Shop Smoke Test", async ({ page }) => {
+  test.setTimeout(120_000);
   await page.goto("http://localhost:5173");
 
   await expect(
